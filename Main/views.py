e477971db39cc01c07ca9ef7a4d06b282bc017b0
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-02-09 01:50:30 +0300

    commit

--- Main/views.py
+++ Main/views.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-#from django.contrib.models import user
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 
 def home(request):
+    return render(request, 'Main/home.html')
+
+def signup(request):
     if request.method == 'POST':
         # Take all the fields and assign them to the users
         username = request.POST['username']
@@ -12,20 +18,28 @@ def home(request):
         Password = request.POST['pass1']
         Password2 = request.POST['pass2']
         
+        myUser = User.objects.create_user(username, emailaddress, Password)
+        myUser.first_name = firstname
+        myUser.last_name = lastname
         
+        myUser.save()
         
-        
-    
-    
-    
-    
-    
-        return render(request, 'Main/home.html')
-
-def signup(request):
-    return render(request, 'Main/signup.html')
+        messages.success(request, "Your Account has been sucessfully created")
+        return redirect('signin')
+    return render(request, 'Main/Signup.html')
 
 def signin(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['pass1']
+        
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+        else: 
+            messages.error(request, 'Wrong Username or password')
+            return render(request,'Main/home.html')
+    
     return render(request, 'Main/signin.html')
 
 def signout(request):
