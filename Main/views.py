057fae8d58d6fbe5468b8fa9a852e8314ec00fd7
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-02-09 01:06:00 +0300

    Update views.py

--- Main/views.py
+++ Main/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+#from django.contrib.models import user
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        # Take all the fields and assign them to the users
+        username = request.POST['username']
+        firstname = request.POST['fname']
+        lastname = request.POST['lname']
+        emailaddress = request.POST['email']
+        Password = request.POST['pass1']
+        Password2 = request.POST['pass2']
+        
+        
+        
+        
+    
+    
+    
+    
+    
+        return render(request, 'Main/home.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    return render(request, 'Main/signup.html')
 
 def signin(request):
-    return render(request, 'signin.html')
+    return render(request, 'Main/signin.html')
 
 def signout(request):
     pass
