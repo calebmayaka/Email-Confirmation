afa1dd5cb5f750b6a8b047b749abe95406b22758
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-02-16 23:02:12 +0300

    Update urls.py

--- Main/urls.py
+++ Main/urls.py
@@ -4,9 +4,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name = 'home'),
-  path('signin', views.signin, name = 'signin'),
-  path('signup', views.signup, name = 'signup'),
-  path('signout', views.signout, name = 'signout'),
-  # path('profile/', views.profile, name = 'profile'),
-  
+  path('signup/',views.signup, name = 'signup'),
+  path('signin/', views.signin, name = 'signin'),
+  path('signout/', views.signout, name = 'signout'),
+ 
 ]
