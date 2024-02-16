8fe32f75e82dbb0e1f1bb65351b5f50fa007e326
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-02-16 23:02:06 +0300

    Update urls.py

--- AuthEmail/urls.py
+++ AuthEmail/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # admin login
     path('admin/', admin.site.urls),
+    # default path
     path ('', include('Main.urls')),
 ]
